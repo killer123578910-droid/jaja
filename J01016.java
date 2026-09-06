cf463c42985201518d9b69ecc376603beb415e95
Khuong <killer123578910@gmail.com>
2026-09-06 23:41:14 +0700

    6/9

---
+++ J01016.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class J01016 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String n = sc.nextLine();
+    int count = 0;
+    for (int i = 0; i < n.length(); i++) {
+      if (n.charAt(i) == '4' || n.charAt(i) == '7') {
+        count += 1;
+      }
+    }
+    System.out.println(count == 4 || count == 7 ? "YES" : "NO");
+  }
+}
